147c4efd24ebd1845fb9af197ac307efd5bec29a
myw219 <myw219@lehigh.edu>
2020-01-22 16:27:39 -0500

    got the current date working

--- src/App.js
+++ src/App.js
@@ -1,32 +1,136 @@
-/* https://youtu.be/GuA0_Z1llYU?t=856 */
+/**
+ * Followed this tutorial
+ * https://www.youtube.com/watch?v=IxuqmfO6p28&t=2s 
+ * */
 
+//Components
 import React from 'react';
+import Weather from './app_components/weather.component';
+import Form from './app_components/form.component';
+import './App.css';
 
+/** Dependencies*/
+import "weather-icons/css/weather-icons.css"; //git project from https://github.com/erikflowers/weather-icons
+import "bootstrap/dist/css/bootstrap.min.css";
+
+/**
+ * Here is the api documentation for openweathermap
+ * https://openweathermap.org/current
+ */
 const apiKey = {
   key: '7b7154eb47e573afde1536472685b7fb',
-  base: 'api.openweathermap.org/data/2.5/'
+  base: 'http://api.openweathermap.org/data/2.5/'
 }
 
-function App() {
+/*const currentDate = (d) => {
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-  const currentDate = (d) => {
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
 
+  return `${day} ${date} ${month} ${year}`
+} */
+
+class App extends React.Component{
+ constructor(){
+   super();
+   this.state={
+     city:undefined,
+     country:undefined,
+     icon:undefined,
+     main:undefined,
+     temp:undefined,
+     temp_max:undefined,
+     temp_min:undefined,
+     description:"",
+     error:false
+   }
+
+   this.weatherIcon={
+    Thunderstorm: "wi-thunderstorm",
+    Drizzle: "wi-sleet",
+    Rain: "wi-storm-showers",
+    Snow: "wi-snow",
+    Atmosphere: "wi-fog",
+    Clear: "wi-day-sunny",
+    Clouds: "wi-day-fog"
+   }
+ } 
+
+ getWeatherIcon(icons, rangeId){
+  switch(true){
+    case rangeId >= 200 && rangeId < 232:
+        this.setState({ icon: icons.Thunderstorm });
+        break;
+      case rangeId >= 300 && rangeId <= 321:
+        this.setState({ icon: icons.Drizzle });
+        break;
+      case rangeId >= 500 && rangeId <= 521:
+        this.setState({ icon: icons.Rain });
+        break;
+      case rangeId >= 600 && rangeId <= 622:
+        this.setState({ icon: icons.Snow });
+        break;
+      case rangeId >= 701 && rangeId <= 781:
+        this.setState({ icon: icons.Atmosphere });
+        break;
+      case rangeId === 800:
+        this.setState({ icon: icons.Clear });
+        break;
+      case rangeId >= 801 && rangeId <= 804:
+        this.setState({ icon: icons.Clouds });
+        break;
+      default:
+        this.setState({ icon: icons.Clouds });
   }
+ }
 
+ getWeather = async(e) => {
 
+  e.preventDefault();
 
-  return (
-    <div className="App">
-      <main>
-        <div className='search-box'>
-          <input type='text' className='search-bar' placeholder="Enter a zip code"></input>
-        </div>
-        <div className='location'> New York City</div>
-        <div className='currentDate'> {currentDate(new Date())} </div>
-        <div className='weather'> 69 F </div>
-      </main>
-    </div>
-  );
+  const city = e.target.elements.city.value;
+  //const country = e.target.elements.country.value;
+
+  if(city){
+    const api_call=await fetch(`${apiKey.base}weather?APPID=${apiKey.key}&q=${city},us&units=imperial`);
+    const res = await api_call.json();
+    console.log(res);
+
+    
+
+  this.setState({
+    city:`${res.name}`,
+    temp:Math.round(res.main.temp),
+    temp_max:Math.round(res.main.temp_max),
+    temp_min:Math.round(res.main.temp_min),
+    description:res.weather[0].description,
+    icon: this.getWeatherIcon(res.weather[0].icon),
+    error:false
+  });
+
+  this.getWeatherIcon(this.weatherIcon, res.weather[0].id);
+  } else {
+    this.setState({error:true})
+  }
+}
+  render(){
+    return(
+      <div className='App'>
+        <Form loadweather={this.getWeather}/>
+        <Weather 
+        city={this.state.city}
+        temp={this.state.temp}
+        temp_max={this.state.temp_max}
+        temp_min={this.state.temp_min}
+        description={this.state.description}
+        weatherIcon={this.state.icon}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/app_components/weather.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+
+const Weather = (props) => {
+    return(
+        <div className='container text-light style = {{height: "100vh"}}'>
+            <div className='cards pt-4'>
+                <h1>{props.city}</h1>
+                <h5 className='py-4'>
+                    <i className={`wi ${props.weatherIcon} display-1`}></i>
+                </h5>
+                {props.temp ? <h1 className='py-2'>{props.temp}&deg;F</h1> : null}
+                {/*shows the min and max temps*/}
+                {minMaxTemp(props.temp_max, props.temp_min)}
+                <h4 className='py-3'>{props.description}</h4>
+            </div>
+        </div>
+    );
+};
+
+function minMaxTemp(max,min){
+    if(min && max){
+        return(
+            <h3>
+            <span className='px-4'>High: {max}&deg;F</span>
+            <span className='px-4'>Low: {min}&deg;F</span>
+            </h3>
+        );
+    }
+}
+
+export default Weather;
