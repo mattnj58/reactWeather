85fce8c70014d32641bf239d010de07375e6408c
myw219 <myw219@lehigh.edu>
2020-01-27 15:42:29 -0500

    redoing forecast component

--- src/app_components/forecast.component.jsx
+++ src/app_components/forecast.component.jsx
@@ -1,27 +1,12 @@
 import React from 'react';
-import PropsTypes from 'prop-types';
 
 const Forecast = (props) =>{
-    console.log(props.forecast)
-    // return props.map(item => (
-    //     <h3>{item.main.temp}</h3>
-    // ));
 
-    return(
-        <div className='container text-light style = {{height: "100vh"}}'>
-            <div className="day">
-                {/* shows the forecast for the next 5 days */}
-                {/* <h4 className='py-3'>{props.forecast[0].}</h4> */}
-                
-                {props.forecast ? <h1 className='py-2'>{this.props.forecast[0].main.temp}&deg;F</h1> : null}
-            </div>
-        </div>
-    );
-} 
+    let forecast=props.forecast;
+    let temps = [];
 
-Forecast.propsTypes = {
-    forecast: PropsTypes.number
+    
+
 }
-
 
 export default Forecast;
