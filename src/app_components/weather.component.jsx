3e6b3b49dcb59a2b2335f613e2bbefd059408125
myw219 <myw219@lehigh.edu>
2020-03-23 14:17:50 -0400

    idk

--- src/app_components/weather.component.jsx
+++ src/app_components/weather.component.jsx
@@ -6,10 +6,10 @@ const Weather = (props) => {
             <div className='cards pt-4'>
                 <h1 className='py-4'>{props.date}</h1>
                 <h1>{props.city}</h1>
-                {props.temp ? <h1 className='py-2'>{props.temp}&deg;F</h1> : null}
                 <h5 className='py-4'>
                     <i className={`wi ${props.weatherIcon} display-1`}></i>
                 </h5>
+                {props.temp ? <h1 className='py-2'>{props.temp}&deg;F</h1> : null}
                 <h4 className='py-3'>{props.description}</h4>
                 {/*shows the min and max temps*/}
                 {minMaxTemp(props.temp_max, props.temp_min)}
